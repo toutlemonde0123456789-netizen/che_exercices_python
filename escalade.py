73a246dde860d80fca7ecf253a9ab0c4d6f11c8e
Honey <toutlemonde0123456789@gmail.com>
2026-09-22 20:05:48 +0200

    Add failure attempts evaluation in escalade.py

---
+++ escalade.py
@@ -0,0 +1,9 @@
+echecs = int (input("Tentatives d'echecs"))
+if echecs < 0
+print ("Erreur : Nombre négatif non autorisé !")
+elif echecs >= 20
+print ("CRITIQUE")
+elif echecs >= 5:
+    print("A SURVEILLER")
+else:
+    print("NORMAL")
