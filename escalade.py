036cb93e87d75a4a48e7fedcc130d851b3f891c5
Honey <toutlemonde0123456789@gmail.com>
2026-09-22 21:18:21 +0200

    Update escalade.py

--- escalade.py
+++ escalade.py
@@ -1,10 +1,1 @@
-Nbr= int 
-print ("Entrez le nombre de tentatives d'echecs")
-if echecs < 0
-print ("Erreur : Nombre négatif non autorisé !")
-elif echecs >= 20
-print ("CRITIQUE")
-elif echecs >= 5:
-print("A SURVEILLER")
-else:
-print("NORMAL")
+
